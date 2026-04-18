23dc52dca00e5624e2c6d6cd49c9d74d5fa65ad9
Jonnie <sfsf0901@gmail.com>
2026-04-18 21:31:00 +0900

    feat: Products 컴포넌트 레이아웃 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import {FaBeer} from "react-icons/fa";
+import Products from "./components/Products.jsx";
 
 function App() {
 
   return (
-      <h1 className="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
-        Welcome <FaBeer />
-      </h1>
+      <div>
+          <Products />
+      </div>
   )
 }
 
