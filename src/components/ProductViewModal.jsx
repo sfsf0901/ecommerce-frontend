7e3ce386c1223b13c41cdafe20383aefd8f54fe1
Jonnie <sfsf0901@gmail.com>
2026-04-22 22:29:32 +0900

    feat: 상품 상세 모달 추가

--- src/components/ProductViewModal.jsx
+++ src/components/ProductViewModal.jsx
@@ -1,7 +1,11 @@
-import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Button, Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Divider } from '@mui/material';
 import { useState } from 'react'
+import { MdClose, MdDone } from 'react-icons/md';
+import Status from "./Status.jsx";
 
 function ProductViewModal({open, setOpen, product, isAvailable}) {
+
     const {id, productName, image, description, quantity, price, discount, specialPrice} = product;
     const handleClickOpen = () => {
         setOpen(true);
@@ -9,26 +13,77 @@ function ProductViewModal({open, setOpen, product, isAvailable}) {
 
     return (
         <>
-            <Dialog open={open} as="div" className="relative z-10 focus:outline-none" onClose={close}>
+            <Dialog open={open} as="div" className="relative z-10" onClose={close}>
+                <DialogBackdrop className="fixed inset-0 bg-gray-500/75 transition-opacity" />
                 <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                     <div className="flex min-h-full items-center justify-center p-4">
                         <DialogPanel
                             transition
-                            className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+                            className="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all md:max-w-[620px] md:min-w-[620px] w-full"
                         >
-                            <DialogTitle as="h3" className="text-base/7 font-medium text-slate-800">
-                                {productName}
-                            </DialogTitle>
-                            <p className="mt-2 text-sm/6 text-slate-800">
-                                {description}
-                            </p>
-                            <div className="mt-4">
-                                <Button
-                                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
+                            {image && (
+                                <div className='flex justify-center aspect-3/2'>
+                                    <img
+                                        src={image}
+                                        alt={productName} />
+                                </div>
+                            )}
+
+                            <div className='px-6 pt-10 pb-2'>
+                                <DialogTitle as="h1" className="lg:text-3xl sm:text-2xl text-xl font-semibold leading-6 text-gray-800 mb-4">
+                                    {productName}
+                                </DialogTitle>
+
+                                <div className="space-y-2 text-gray-700 pb-4">
+                                    <div className="flex items-center justify-between gap-2">
+                                        {specialPrice ? (
+                                            <div className="flex items-center gap-2">
+                                                <span className="text-gray-400 line-through">
+                                                    ${Number(price).toFixed(2)}
+                                                </span>
+                                                <span className="sm:text-xl font-semibold text-slate-700">
+                                                    ${Number(specialPrice).toFixed(2)}
+                                                </span>
+                                            </div>
+                                        ) : (
+                                            <span className="text-xl font-bold">
+                                                {" "}
+                                                ${Number(price).toFixed(2)}
+                                            </span>
+                                        )}
+
+                                        {isAvailable ? (
+                                            <Status
+                                                text="In Stock"
+                                                icon={MdDone}
+                                                bg="bg-teal-200"
+                                                color="text-teal-900"
+                                            />
+                                        ) : (
+                                            <Status
+                                                text="Out-Of-Stock"
+                                                icon={MdClose}
+                                                bg="bg-rose-200"
+                                                color="text-rose-700"
+                                            />
+                                        )}
+                                    </div>
+
+                                    <Divider />
+
+                                    <p>{description}</p>
+                                </div>
+                            </div>
+
+
+                            <div className="px-6 py-4 flex justify-end gap-4">
+                                <button
                                     onClick={() => setOpen(false)}
+                                    type="button"
+                                    className="px-4 py-2 text-sm font-semibold text-slate-700 border border-slate-700 hover:text-slate-800 hover:border-slate-800 rounded-md cursor-pointer"
                                 >
-                                    Got it, thanks!
-                                </Button>
+                                    Close
+                                </button>
                             </div>
                         </DialogPanel>
                     </div>
