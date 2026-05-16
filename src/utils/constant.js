acb24b9cd6b3dba39809b0f65ea25705f29d2ab7
Jonnie <sfsf0901@gmail.com>
2026-05-16 16:31:01 +0900

    feat: Swiper를 활용한 HeroBanner 슬라이더 구현

---
+++ src/utils/constant.js
@@ -0,0 +1,9 @@
+import bannerImageOne from "../assets/sliders/s_1.webb"
+import bannerImageTwo from "../assets/sliders/s_2.webb"
+import bannerImageThree from "../assets/sliders/s_3.webb"
+
+export {
+    bannerImageOne,
+    bannerImageTwo,
+    bannerImageThree,
+}
