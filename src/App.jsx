582c076554cb9345d8fd3a09b5cee346b85633c4
Jonnie <sfsf0901@gmail.com>
2026-05-17 14:43:32 +0900

    feat: Navbar 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Products from "./components/products/Products.jsx";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/home/Home.jsx";
+import Navbar from "./components/shared/Navbar.jsx";
 
 function App() {
 
   return (
       <Router>
+          <Navbar />
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/products" element={<Products />} />
