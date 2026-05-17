21cbd475a85a71c7494f9430be3aa925cb90bf31
Jonnie <sfsf0901@gmail.com>
2026-05-17 15:29:01 +0900

    feat: About 페이지 추가, ProductCard about 모드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Products from "./components/products/Products.jsx";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import Navbar from "./components/shared/Navbar.jsx";
+import About from "./components/About.jsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/about" element={<About />} />
           </Routes>
       </Router>
   )
