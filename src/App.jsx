04c7bc7a4eafdceb34ae920de58fe22bd2a8db77
Jonnie <sfsf0901@gmail.com>
2026-05-17 21:55:01 +0900

    feat: Contact 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import Navbar from "./components/shared/Navbar.jsx";
 import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/products" element={<Products />} />
               <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
           </Routes>
       </Router>
   )
