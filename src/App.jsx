8c0172b6ae3870e7902461bb00c9b7d1062a16b3
Jonnie <sfsf0901@gmail.com>
2026-05-18 22:18:22 +0900

    feat: 장바구니 기능 추가 및 토스트 알림 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
+import React from "react";
 import './App.css'
 import Products from "./components/products/Products.jsx";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import Navbar from "./components/shared/Navbar.jsx";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
 
   return (
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-          </Routes>
-      </Router>
+      <React.Fragment>
+          <Router>
+              <Navbar />
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+              </Routes>
+          </Router>
+          <Toaster position='bottom-center' />
+      </React.Fragment>
   )
 }
 
