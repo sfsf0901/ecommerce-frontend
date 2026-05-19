5459402e085552e74237d44a8367c30855072597
Jonnie <sfsf0901@gmail.com>
2026-05-19 22:18:31 +0900

    feat: Cart 페이지 통화 단위 수정

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -36,7 +36,7 @@ const Cart = () => {
                 <div className="flex text-sm gap-1 flex-col">
                     <div className="flex justify-between w-full md:text-lg text-sm font-semibold">
                         <span>Subtotal</span>
-                        <span>$400</span>
+                        <span>₩400</span>
                     </div>
 
                     <p className="text-slate-500">
