e331c705454c68967954d132f63c1721360cb3ab
Jonnie <sfsf0901@gmail.com>
2026-05-27 20:42:29 +0900

    feat: 빈 장바구니 화면 추가

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,6 +2,7 @@ import {MdArrowBack, MdShoppingCart} from "react-icons/md";
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import ItemContent from "./ItemContent.jsx";
+import CartEmpty from "./CartEmpty.jsx";
 
 const Cart = () => {
     const dispatch = useDispatch();
@@ -13,7 +14,7 @@ const Cart = () => {
     )
 
     if (!cart || cart.length === 0) {
-        return <h1>Cart is Empty</h1>
+        return <CartEmpty />
     }
 
     return (
