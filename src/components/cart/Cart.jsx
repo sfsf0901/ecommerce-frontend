30c1e5410ab9071c5aad8a5345498e8f78a04286
Jonnie <sfsf0901@gmail.com>
2026-05-28 19:18:38 +0900

    feat: 장바구니 합계 금액 계산 로직 추가

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import ItemContent from "./ItemContent.jsx";
 import CartEmpty from "./CartEmpty.jsx";
+import {formatPrice} from "../../utils/formatPrice.js";
 
 const Cart = () => {
     const dispatch = useDispatch();
@@ -10,7 +11,7 @@ const Cart = () => {
     const newCart = { ...cart };
 
     newCart.totalPrice = cart?.reduce(
-        (acc, cur) => acc * Number(cur?.specialPrice) * Number(cur?.quantity), 0
+        (acc, cur) => acc + Number(cur?.specialPrice) * Number(cur?.quantity), 0
     )
 
     if (!cart || cart.length === 0) {
@@ -56,7 +57,7 @@ const Cart = () => {
                 <div className="flex text-sm gap-1 flex-col">
                     <div className="flex justify-between w-full md:text-lg text-sm font-semibold">
                         <span>Subtotal</span>
-                        <span>₩400</span>
+                        <span>{formatPrice(newCart?.totalPrice)}</span>
                     </div>
 
                     <p className="text-slate-500">
